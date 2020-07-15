219177ac58de00d0004c1cc5b701a44d912e2723
Raymond Tao <21taoray@hawken.edu>
2020-07-15 13:46:06 -0400

    added type hint for Bot, hope it works

--- cogs/.TEMPLATE.py
+++ cogs/.TEMPLATE.py
@@ -2,14 +2,16 @@
 
 from discord.ext.commands import command, Cog, Context
 
+from bot import Bot
+
 
 class TEMPLATE(Cog):
     """The description for TEMPLATE goes here."""
 
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
-    # @COMMA()
+    # @COMMAND()
     # async defND COMMAND(self, ctx: Context):
     #     """The description for COMMAND goes here."""
     #     pass
