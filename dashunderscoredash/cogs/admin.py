6a3287590e92e599063addb2e24ea58acf5e013c
Raymond Tao <21taoray@hawken.edu>
2020-08-26 22:03:15 -0400

    rewrite to use embed fields instead of text description

--- dashunderscoredash/cogs/admin.py
+++ dashunderscoredash/cogs/admin.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from discord import Embed, Member, TextChannel, Role
 from discord.ext.commands import command, Cog, Bot, Context
 
@@ -12,68 +14,81 @@ class Admin(Cog):
     async def guild(self, ctx: Context):
         """Information about the current guild (server)."""
         guild = ctx.guild
+
+        embed = Embed()
         if guild is None:
-            await ctx.send(embed=Embed(
-                title="No Guild Found",
-                description="Are you sure you're invoking this command in the context of a guild?",
-            ))
+            embed.title = "No Guild Found"
+            embed.description = "Are you sure you're invoking this command in a guild (server)?"
         else:
-            await ctx.send(embed=Embed(
-                title=f"Guild: {guild.name}",
-                description=f"ID: {guild.id}\n"
-                            f"Owner: {guild.owner.name if guild.owner is not None else 'None'}\n"
-                            f"Region: {guild.region}\n"
-                            f"Channels: {len(guild.channels) - len(guild.categories)}\n"
-                # TODO: use Guild.member_count instead of len(guild.members)
-                            f"Members: {len(guild.members)}\n"
-                            f"Roles: {len(guild.roles)}\n"
-                            f"Description: {guild.description}\n"
-            ))
+            embed.title = f"Guild: {guild.name}"
+            embed.add_field(name="ID", value=guild.id)
+            embed.add_field(name="Created", value=guild.created_at.strftime('%x %X') + " UTC")
+            embed.add_field(name="Owner", value=(guild.owner.name if guild.owner is not None else "None"))
+            embed.add_field(name="Region", value=guild.region)
+            embed.add_field(name="# Channels", value=str(len(guild.channels) - len(guild.categories)))
+            embed.add_field(name="# Members", value=guild.member_count)
+            embed.add_field(name="# Roles", value=str(len(guild.roles)))
+            embed.add_field(name="Description", value=guild.description)
+
+        await ctx.send(embed=embed)
 
     @command()
-    async def role(self, ctx: Context, role: Role):
-        """Information about the given role."""
-        await ctx.send(embed=Embed(
-            title=f"Role: @{role.name}",
-            description=f"ID: {role.id}\n"
-                        f"Separate: {role.hoist}\n"
-                        f"Managed: {role.managed}\n"
-                        f"Mentionable: {role.mentionable}\n"
-                        f"Created at: {role.created_at} UTC\n"
-                        f"Members: {len(role.members)}\n",
-        ))
+    async def role(self, ctx: Context, role: Optional[Role] = None):
+        """Information about your highest role or any given role."""
+
+        if role is None:
+            role = ctx.author.top_role
+
+        embed = Embed()
+        embed.title=f"Role: @{role.name}"
+        embed.add_field(name='ID', value=role.id)
+        embed.add_field(name='Separate', value=("Yes" if role.hoist else "No"))
+        embed.add_field(name='Managed', value=("Yes" if role.managed else "No"))
+        embed.add_field(name='Mentionable', value=("Yes" if role.mentionable else "No"))
+        embed.add_field(name='Created', value=role.created_at.strftime('%x %X') + ' UTC')
+        embed.add_field(name='# Members', value=str(len(role.members)))
+
+        await ctx.send(embed=embed)
 
     @command()
     async def channel(self, ctx: Context, channel: TextChannel = None):
         """Information about this text channel or any other on the server."""
         # TODO: make it work for more than just TextChannel
+
         if channel is None:
             channel = ctx.channel
-        await ctx.send(embed=Embed(
-            title=f"Channel: #{channel}",
-            description=f"Category: {channel.category}\n"
-                        f"Created at: {channel.created_at} UTC\n"
-                        f"Changed roles: {[role.name for role in channel.changed_roles]}\n"
-                        f"Permissions synced: {channel.permissions_synced}\n",
-        ))
+
+        embed = Embed()
+        embed.title = f"Channel: #{channel}"
+        embed.add_field(name='Category', value=channel.category)
+        embed.add_field(name='Created', value=channel.created_at.strftime('%x %X') + ' UTC')
+        embed.add_field(name='Changed Roles', value=str(len(channel.changed_roles)))
+        embed.add_field(name='Permissions Synced', value=("Yes" if channel.permissions_synced else "No"))
+
+        await ctx.send(embed=embed)
 
     @command(aliases=['user'])
-    async def member(self, ctx: Context, member: Member = None):
+    async def member(self, ctx: Context, member: Optional[Member] = None):
         """Information about yourself or any other server member."""
+
         if member is None:
+            # default to self
             member = ctx.author
-        await ctx.send(embed=Embed(
-            title=f"Member: {member} (@{member.display_name})",
-            description=f"ID: {member.id}\n"
-                        f"Status: {member.status}\n"
-                        f"Activity: {member.activity}\n"
-                        f"Top role: {member.top_role}\n"
-                        f"Voice state: {member.voice}\n"
-                        f"Created at: {member.created_at} UTC\n"
-                        f"Joined at: {member.joined_at} UTC\n"
-                        f"Bot account: {member.bot}\n"
-                        f"System account: {member.system}\n",
-        ))
+
+        embed = Embed()
+        embed.title = f"{member} (@{member.display_name})"
+        embed.set_thumbnail(url=member.avatar_url)
+        embed.add_field(name='User ID', value=member.id)
+        embed.add_field(name='Status', value=member.status)
+        embed.add_field(name='Activity', value=member.activity)
+        embed.add_field(name='Top Role', value=member.top_role)
+        embed.add_field(name='Voice', value=member.voice)
+        embed.add_field(name='Created', value=member.created_at.strftime('%x %X') + ' UTC')
+        embed.add_field(name='Joined', value=member.joined_at.strftime('%x %X') + ' UTC')
+        embed.add_field(name='Bot', value=("Yes" if member.bot else "No"))
+        embed.add_field(name='System Account', value=("Yes" if member.system else "No"))
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
